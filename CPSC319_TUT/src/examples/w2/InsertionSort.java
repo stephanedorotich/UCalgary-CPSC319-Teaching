7c908eb0298517adbc696cb643f6d3c737ad8f24
stephanedorotich <stephanedorotich@gmail.com>
2022-07-07 13:41:56 -0600

    w2_t2 - bugfix for dsc_nums in ListCreator

--- CPSC319_TUT/src/examples/w2/InsertionSort.java
+++ CPSC319_TUT/src/examples/w2/InsertionSort.java
@@ -1,29 +1,5 @@
 package examples.w2;
 
-import java.util.Random;
-
-class ListCreator {
-	int[] rand_nums(int n) {
-		Random rand = new Random();
-		int[] myList = new int[n];
-		for (int i = 0; i < n; i++) myList[i] = rand.nextInt(65536);
-		return myList;
-	}
-	
-	int[] asc_nums(int n) {
-		int[] myList = new int[n];
-		for (int i = 0; i < n; i++) myList[i] = i;
-		return myList;
-	}
-	
-	int[] dsc_nums(int n) {
-		int[] myList = new int[n];
-		for (int i = n; i > 0; i--) myList[i] = i;
-		return myList;
-	}
-}
-
-
 public class InsertionSort {
 	private static void print(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
@@ -45,9 +21,10 @@ public class InsertionSort {
 	}
 	
 	public static void main(String[] args) {
-		ListCreator lc = new ListCreator();
-		int[] nums = lc.rand_nums(10);
+		int[] nums = ListCreator.rand_nums(10);
 		
 		insertionSort(nums);
+		
+		print(nums);
 	}
 }
---
+++ CPSC319_TUT/src/examples/w2/ListCreator.java
@@ -0,0 +1,28 @@
+package examples.w2;
+
+import java.util.Random;
+
+public class ListCreator {
+	public static int[] rand_nums(int n) {
+		Random rand = new Random();
+		int[] myList = new int[n];
+		for (int i = 0; i < n; i++) myList[i] = rand.nextInt(65536);
+		return myList;
+	}
+	
+	public static int[] asc_nums(int n) {
+		int[] myList = new int[n];
+		for (int i = 0; i < n; i++) myList[i] = i;
+		return myList;
+	}
+	
+	public static int[] dsc_nums(int n) {
+		int[] myList = new int[n];
+		int val = n;
+		for (int i = 0; i < n; i++) {
+			myList[i] = val;
+			val--;
+		}
+		return myList;
+	}
+}
